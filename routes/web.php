63700bfb3351ba40b505c9cec9577db97fc1e0b1
mfa008 <143124567+mfa008@users.noreply.github.com>
2025-02-12 07:15:20 +0000

    Ajout de la route Acceuil

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Declaration de la route acceuil
+Route::get('Acceuil', [PostController::class, 'Acceuil'])->name('Acceuil');
