5b468ec46785c4c031f226888f34a3b2228ecf7d
mfa008 <143124567+mfa008@users.noreply.github.com>
2025-03-10 14:01:23 +0000

    ajout des lien bootstrap

--- resources/views/View_candidats.blade.php
+++ resources/views/View_candidats.blade.php
@@ -2,6 +2,8 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
   <title>Liste des Candidats</title>
   <style>
     body {
